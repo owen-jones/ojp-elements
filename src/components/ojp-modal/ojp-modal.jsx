35dce07e42a835a020cb0d284c41d2411a816100
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2022-12-09 13:20:29 -0800

    Added element example

--- src/components/ojp-modal/ojp-modal.jsx
+++ src/components/ojp-modal/ojp-modal.jsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, getAssetPath } from '@stencil/core';
+import { Component, Host, h, Prop, getAssetPath, Element } from '@stencil/core';
 
 @Component({
   tag: 'ojp-modal',
@@ -10,31 +10,56 @@ import { Component, Host, h, Prop, getAssetPath } from '@stencil/core';
 export class OjpModal {
 // Props!
 
+  /**
+   * 2. Reference to host HTML element.
+   * Inlined decorator
+   */
+  @Element() el;
+
   // Close icon svg
   @Prop() closeIcon = 'close.svg';
 
 
   /**
-  * Modal is open or opening (css transition)
-  * Type: boolean
-  */
-   @Prop({
+   * Modal is open or opening (css transition)
+   * Type: boolean
+   */
+  @Prop({
     reflect: true,
-    mutable: false
+    mutable: false,
   }) open = false;
- 
+
+  close() {
+    //do the closing here
+    this.el.shadowRoot.dispatchEvent(new CustomEvent('close'));
+  }
+
+
+  componentDidLoad() {
+    this.closeButton = this.el.shadowRoot.querySelector('.close-button');
+
+    this.closeButton.addEventListener('click', () => {
+      this.close();
+    });
+  }
+
+
   render() {
     return (
       <Host>
-        <div class="ojp-modal-wrapper">
-          <div class="ojp-modal-overlay"/>
-            <div class="ojp-modal-panel">
-              <div class="ojp-modal-close">
-                <img src={getAssetPath('./assets/${this.closeIcon}')} alt="Close Icon"/>
-              </div>
-              <slot></slot>
+        <div class='ojp-modal-wrapper'>
+          <div class='ojp-modal-overlay' />
+          <div class='ojp-modal-panel'>
+            <div class='ojp-modal-close'>
+              <button class='close-button'>
+                <slot name='close-icon'>
+                  <img src={getAssetPath('./assets/${this.closeIcon}')} alt='Close Icon' />
+                </slot>
+              </button>
             </div>
+            <slot></slot>
           </div>
+        </div>
       </Host>
     );
   }
