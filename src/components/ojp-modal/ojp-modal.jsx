3170821245a2013ce62d33f6ac7876d2c74a58cc
Parth <parth.shiralkar@gmail.com>
2022-12-27 20:21:59 -0800

    Change button to hyperlink

--- src/components/ojp-modal/ojp-modal.jsx
+++ src/components/ojp-modal/ojp-modal.jsx
@@ -118,11 +118,11 @@ export class OjpModal {
         <div class={this.open ? "ojp-modal-wrapper is-open" : "ojp-modal-wrapper"}>
           <div class='ojp-modal-overlay' />
           <div class='ojp-modal-close'>
-            <button class='close-button' tabindex='0'>
+            <a class='close-button' tabindex='0'>
               <slot name='close-icon'>
                 <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="20px" height="20px"><path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z" /></svg>
               </slot>
-            </button>
+            </a>
           </div>
           <div class='ojp-modal-panel'
             id="alert-modal"
