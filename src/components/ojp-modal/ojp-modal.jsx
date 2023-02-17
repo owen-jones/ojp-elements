05d544b4d6dee2e28a456510e4c8bdfe4d79866a
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2023-02-17 11:49:54 -0800

    Fixes modal width issue that pushes content outside the left/right boundaries

--- src/components/ojp-modal/ojp-modal.jsx
+++ src/components/ojp-modal/ojp-modal.jsx
@@ -111,10 +111,12 @@ export class OjpModal {
     if(isVisible) {
       document.body.style.top = `-${window.scrollY}px`;
       document.body.style.position = 'fixed';
+      document.body.style.width = '100%';
     } else {
       const scrollY = document.body.style.top;
       document.body.style.position = '';
       document.body.style.top = '';
+      document.body.style.width = '';
       window.scrollTo(0, parseInt(scrollY || '0') * -1);
     }
   }
